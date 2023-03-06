9d9ea28ab803ba7ac908b32b1225287dd5202fea
Nick O'Bryan <obryannp@gmail.com>
2023-03-05 21:31:20 -0600

    Worked on all functions of SortedList and halfway through final tests

--- Program 3/SortedList.cpp
+++ Program 3/SortedList.cpp
@@ -34,22 +34,27 @@ SortedList::SortedList(int userMaxArraySize)
 SortedList::~SortedList()
 {
 	MakeEmpty();
+	//delete itemList;
 }
 
 //copy constructor
 void SortedList::operator = (const SortedList& listToCopy)
 {
+	//placeholder variables
+	int tempNum;
+	string tempStr;
 
+	//length of original array
 	int copyLength = listToCopy.length;
 
-	//SortedList* tempList = new SortedList(copyLength);
-	SortedList tempList;
-
+	//iterates over list and deep copies all items to new list
 	for (int index = 0; index < copyLength; index++)
 	{
-		tempList.itemList[index] = listToCopy.itemList[index];
+		tempNum = listToCopy.itemList[index].GetData(tempStr);
+		itemList[index].SetData(tempNum, tempStr);
 	}
 
+	//copies all other info from original list to new list, sets curPos to 0
 	MaxArraySize = listToCopy.MaxArraySize;
 	length = listToCopy.length;
 	curPos = 0;
@@ -58,6 +63,7 @@ void SortedList::operator = (const SortedList& listToCopy)
 
 bool SortedList::PutItem(ItemType userItem)
 {
+	cout << "PutItem():   ";
 	//variables
 	int iterator = 0;
 
@@ -82,6 +88,7 @@ bool SortedList::PutItem(ItemType userItem)
 		//if list already has items
 		else
 		{
+			//added these so conditional statements are easier to read
 			bool tempA = GetLength() < MaxArraySize;
 			bool tempB = itemList[iterator] < userItem;
 			bool tempC = iterator >= GetLength();
@@ -94,6 +101,8 @@ bool SortedList::PutItem(ItemType userItem)
 				tempC = iterator >= GetLength();
 				if (tempC)
 				{
+					string tempStr;
+					cout << "Added: " << userItem.GetData(tempStr) << "  " << tempStr << "\n";
 					itemList[iterator] = userItem;
 					length++;
 					return true;
@@ -102,54 +111,86 @@ bool SortedList::PutItem(ItemType userItem)
 
 			int indexToAdd = iterator;
 
+			//iterates through list and moves items out to make space for new item
 			for (int index = GetLength(); index > indexToAdd; index--)
 			{
 				itemList[index] = itemList[index - 1];
 			}
 
+			//adds new item to the list in open spot
 			itemList[indexToAdd] = userItem;
 		}
 
+		//increments length and prints statement to user
+		string tempStr;
+		cout << "Added: " << userItem.GetData(tempStr) << "  " << tempStr << "\n";
 		length++;
 		return true;
 	}
 
+	cout << "Item is already in the list.\n";
 	return false;
 }
 
+//function that searches for item in the list
 ItemType SortedList::GetItem(ItemType userItem, bool& found)
 {
+	//default itemtype to return if item not found
+	ItemType defaultItem;
+
+	//checks if the list is initialized and there are items in the list
 	if (MaxArraySize != 0 && length != 0)
 	{
+		//runs if item is not found in binary search
 		if (BinarySearch(itemList, userItem, 0, length) < 0)
 		{
 			cout << "Item not found.\n";
 			found = false;
-			return userItem;
+			return defaultItem;
 		}
-
+		//runs if item is found
 		cout << "Item was found.\n";
 		found = true;
 		return userItem;
 	}
 
-	cout << "Item not found.\n";
+	cout << "Item not found, array has no items.\n";
 	found = false;
-	return userItem;
+	return defaultItem;
 }
 
 bool SortedList::DeleteItem(ItemType userItem)
 {
-	length--;
-	return true;
+	cout << "\nDeleteItem():\n";
+	//this is true if item is in the list
+	int indexToRemove = BinarySearch(itemList, userItem, 0, length);
+	if (indexToRemove >= 0)
+	{
+		//moves all items over and deletes item to be deleted
+		for (int index = indexToRemove; index < length; index++)
+		{
+			itemList[index] = itemList[index + 1];
+		}
+
+		//decrements length and returns true
+		cout << "Deleted, Stock Number: " << userItem.GetData() << "\n";
+		length--;
+		return true;
+	} 
+
+	//if item is not found/list empty
+	cout << "Item not found.\n";
+	return false;
 }
 
+//function that resets list
 ItemType SortedList::ResetList()
 {
-	ItemType item1;
-	return item1;
+	curPos = 0;
+	return itemList[curPos];
 }
 
+//function that makes the list empty and sets length to 0
 void SortedList::MakeEmpty()
 {
 	for (int index = 0; index < MaxArraySize; index++)
@@ -161,38 +202,48 @@ void SortedList::MakeEmpty()
 	curPos = 0;
 }
 
+//iterates over list and prints each item
 void SortedList::PrintList()
 {
+	cout << "\nPrintList()\n";
+	//temporary string variable
+	string tempStr;
+
+	//iterate list
 	for (int index = 0; index < length; index++)
 	{
-		cout << this->itemList[index].GetData() << " ";
+		cout << this->itemList[index].GetData(tempStr) << "   " << tempStr << "  --  ";
 	}
-	cout << "length: " << length << " curPos: " << curPos << "\n";
+	cout << "\n" << endl;
 }
 
 ItemType SortedList::GetNext()
 {
-	ItemType item1;
-	return item1;
+	curPos++;
+	return itemList[curPos];
 }
 
+
+//function that returns variable length
 int SortedList::GetLength()
 {
 	return length;
 }
 
+//binary search function that searches through list to find a specific stock number
 int SortedList::BinarySearch(ItemType* array, ItemType userItem, int low, int high)
 {
-	bool found;
-
+	//if item is not in the list, returns -1
 	if (low > high)
 	{
 		return -1;
 	}
 
+	//recursive aspect
 	else
 	{
 		int middle = (low + high) / 2;
+		cout << "Searching... " << array[middle].GetData() << "   ";
 		if (userItem == array[middle])
 		{
 			return middle;
@@ -208,5 +259,5 @@ int SortedList::BinarySearch(ItemType* array, ItemType userItem, int low, int hi
 			return BinarySearch(array, userItem, low, middle - 1);
 		}
 	}
-
+	cout << "\n";
 }
--- Program 3/Test_Driver.cpp
+++ Program 3/Test_Driver.cpp
@@ -35,38 +35,85 @@ int main()
 {
 	//SortedList tests
 	SortedList list1;
-	SortedList list2;
-	ItemType item1(10, "1st");
-	ItemType item2(9, "2nd");
-	ItemType item3(8, "3rd");
-	ItemType item4(7, "4th");
-	ItemType item5(6, "5th");
-	ItemType item6(5, "6th");
-	ItemType item7(4, "7th");
-	ItemType item8(3, "7th");
-	ItemType item9(2, "7th");
-	ItemType item10(1, "7th");
-	//ItemType item11(11, "7th");
+	SortedList list2(12);
+	ItemType item1(1, "1st");
+	ItemType item2(2, "2nd");
+	ItemType item3(3, "3rd");
+	ItemType item4(4, "4th");
+	ItemType item5(5, "5th");
+	ItemType item6(6, "6th");
+	ItemType item7(7, "7th");
+	ItemType item8(8, "8th");
+	ItemType item9(9, "9th");
+	ItemType item10(10, "10th");
+	ItemType item11(11, "11th");
+	ItemType item12(12, "12th");
+	ItemType item13(13, "13th");
 	
-	list1.PutItem(item1);
+	//adds items out of order into the right order
+	cout << "ORDER CHECK:\n";
 	list1.PutItem(item1);
 	list1.PutItem(item3);
 	list1.PutItem(item2);
+	list1.PrintList();
+
+	//adds item already in list
+	cout << "\nALREADY IN LIST:\n";
+	list1.PutItem(item1);
+
+	//checks bounds of array
+	cout << "\nCHECK MAX ITEMS:\n";
 	list1.PutItem(item4);
 	list1.PutItem(item5);
 	list1.PutItem(item6);
 	list1.PutItem(item7);
 	list1.PutItem(item8);
 	list1.PutItem(item9);
-	list1.PutItem(item1);
-	//list1.PutItem(item10);
-	//list1.PutItem(item11);
+	list1.PutItem(item10);
+	list1.PutItem(item11);
 
 	list1.PrintList();
 
-	list1 = list2;
-
+	//copy constructor tests
+	/*cout << "\nCOPY CONSTRUCTOR TESTS: \n";
+	list2 = list1;
+	cout << "list1:";
 	list1.MakeEmpty();
 	list1.PrintList();
+	cout << "list2:";
+	list2.PrintList();*/
+	
+	//tests with different max items for list
+	cout << "\nCUSTOM MAX ITEMS CHECK(12 MAX ITEMS)\n";
+	list2.PutItem(item1);
+	list2.PutItem(item3);
+	list2.PutItem(item2);
+	list2.PutItem(item4);
+	list2.PutItem(item5);
+	list2.PutItem(item6);
+	list2.PutItem(item7);
+	list2.PutItem(item8);
+	list2.PutItem(item9);
+	list2.PutItem(item10);
+	list2.PutItem(item11);
+	list2.PutItem(item12);
+	list2.PutItem(item13);
+
 	list2.PrintList();
+
+
+
+	//cout << "\nRESET LIST CHECKS\n" << list2.ResetList().GetData();
+
+	list1.MakeEmpty();
+	list1.PutItem(item1);
+	list1.PutItem(item3);
+	list1.PutItem(item2);
+	list1.PrintList();
+	cout << "\n";
+	list1.DeleteItem(item1);
+	list1.DeleteItem(item1);
+	list1.PrintList();
+	//list2.PrintList();
+
 }
